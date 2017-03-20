52ba4b88234d9a3b34724eb9b11850846da6c026
Jan Schöpke <janschoepke@ymail.com>
2017-03-20 14:46:54 +0100

    Added first implementation of SCSS version. Added Gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,25 @@
 "use strict";
 
 var gulp = require("gulp"),    
-    less = require("gulp-less");
+    less = require("gulp-less"),
+	sass = require("gulp-sass");
 
 gulp.task("less", function(){
-	return gulp.src('./src/pure-extensions.less')
+	return gulp.src('./less/pure-extensions.less')
 				.pipe(less({
-					filename: 'pure-extensions-min.css',
+					filename: 'pure-extensions-less-min.css',
 					compress: true	
 				}))
 			    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task("default", ["less"]);
+gulp.task('sass', function () {
+    return gulp.src('./sass/pure-extensions.scss')
+        .pipe(sass({
+        	file: 'pure-extensions-sass-min.css',
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task("default", ["sass"]);
